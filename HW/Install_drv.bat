5f98b80044afb9ac7116be868cbd63b95d25499d
Mato <comnikist@me.com>
2014-08-24 20:12:46 +0900

    wdapi1020 버전 업

--- HW/Install_drv.bat
+++
@@ -1,5 +0,0 @@
-wdreg -inf windrvr6.inf install
-pause
-wdreg -inf mvs_edio32l4_pci.inf install
-pause
-copy wdapi1010.dll %WINDIR%\system32
--- Install_drv.bat
+++ Install_drv.bat
@@ -1,12 +1,8 @@
 wdreg -inf windrvr6.inf install
 pause
 
-wdreg -inf windrvr6.inf uninstall
+wdreg -inf mvs_edio32l4_pci.inf install
 pause
+
 
-wdreg -inf .\HW\mvs_edio32l4_pci.inf install
-pause
-wdreg -inf .\HW\windrvr6.inf install
-pause
-
-copy wdapi1010.dll %WINDIR%\system32
+copy wdapi1020.dll %WINDIR%\system32
