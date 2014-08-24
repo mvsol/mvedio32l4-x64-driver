ff5b2ef3cff0cd7cb51675bb2f61b1a7c8b833cb
Mato <comnikist@me.com>
2014-08-24 20:09:52 +0900

    Jungo로 표시되는 것 수정

--- Install_drv.bat
+++ Install_drv.bat
@@ -1,5 +1,12 @@
 wdreg -inf windrvr6.inf install
+pause
+
+wdreg -inf windrvr6.inf uninstall
+pause
+
+wdreg -inf .\HW\mvs_edio32l4_pci.inf install
 pause
-wdreg -inf mvs_edio32l4_pci.inf install
-pause
+wdreg -inf .\HW\windrvr6.inf install
+pause
+
 copy wdapi1010.dll %WINDIR%\system32
